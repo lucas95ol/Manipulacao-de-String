41ad76aaccbb39c8feba1686dde1c05183c1a972
Lucas Oliveira <lucasbarbosadeoliveira95@gmail.com>
2023-07-09 16:20:33 -0300

    second commit

--- reverterOrdemFrase.dart
+++ reverterOrdemFrase.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 void main() {
   print("Digite uma frase e irei inverter a ordem das palavras:");
+  //input para receber a frase
   String frase = stdin.readLineSync()!;
+  //lista para guardar as palavras da frase de forma separada
   var palavras = frase.split(" ");
+  //lista que inverte a ordem das palavras
   var palavrasInvertidas = palavras.reversed;
-
+  //imprimindo os elementos da segunda lista em forma de frase
   print(palavrasInvertidas.join(" "));
 }
