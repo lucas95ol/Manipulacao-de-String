a23593a61aa706a6b8e0af77ce104311c9ede31e
Lucas Oliveira <lucasbarbosadeoliveira95@gmail.com>
2023-07-09 16:28:30 -0300

    second commit

--- letraMaiuscula.dart
+++ letraMaiuscula.dart
@@ -1,18 +1,20 @@
 import 'dart:io';
 
-void main(List<String> args) {
+void main() {
   print(
       "Escreva um parágrafo com letras minúsculas e o algoritmo irá corrigir o tamanho das letras:");
+  //input para receber o paragrafo    
   String frase = stdin.readLineSync()!;
-
+  //substituindo a primeira letra digitada por ela mesmo maiuscula
   frase = frase.replaceFirst(frase[0], frase[0].toUpperCase());
-
+  //laço para percorrer os elementos da frase
   for (int i = 0; i < frase.length; i++) {
+    //verificando onde começa uma nova frase dentro do paragrafo
     if (frase[i] == "." && i + 2 < frase.length ||
         frase[i] == "?" && i + 2 < frase.length ||
         frase[i] == "!" && i + 2 < frase.length ||
         frase[i] == ";" && i + 2 < frase.length) {
-      //frase = frase.replaceFirst(frase[i], frase[i].toUpperCase());
+      //se houver pontuação e mais alguma letra depois dela, substitua a letra por ela mesmo maiuscula  
       frase = frase.replaceRange(i + 2, i + 3, frase[i + 2].toUpperCase());
     }
   }
