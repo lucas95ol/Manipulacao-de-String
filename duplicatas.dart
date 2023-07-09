8f3e5e7099700a4f4c7b574dc201b347ec51a7e5
Lucas Oliveira <lucasbarbosadeoliveira95@gmail.com>
2023-07-09 16:16:42 -0300

    second commit

--- duplicatas.dart
+++ duplicatas.dart
@@ -1,8 +1,10 @@
 void main() {
   String texto = "Hello World";
+  //guardando os elementos do texto em uma lista
   var letras = texto.split("");
+  //criando outra lista onde os elementos repetidos da lista anterior foram apagados
   var semRepeticao = Set.from(letras);
-
+  //agora junto os elementos dessa nova lista para formar o novo texto
   String novoTexto = semRepeticao.join();
 
   print(novoTexto);
